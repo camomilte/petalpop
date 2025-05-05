dcccdb79c87e9312fec8a3563c228d191de712b9
Camomilte <camomilte@gmail.com>
2025-05-05 13:51:58 +0200

    added custom fonts properly

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
-import { Gabriela } from 'next/font/google';
-
-const gabriela = Gabriela ({
-    subsets: ['latin'],
-    weight: '400',
-    variable: '--font-gabriela',
-});
-
 const Header = () => {
   return (
-    <div className={`bg-hot text-center py-10 flex justify-around ${gabriela.variable}`}>
-        <h2 className='text-xl font-gabriela'>Events</h2>
-        <h2 className='text-xl font-gabriela'>About</h2>
-        <h2 className='text-xl font-gabriela'>Contact</h2>
+    <div className='bg-hot text-center py-10 flex justify-around text-xl font-gabriela'>
+        <h2>Events</h2>
+        <h2>About</h2>
+        <h2>Contact</h2>
     </div>
   )
 }
